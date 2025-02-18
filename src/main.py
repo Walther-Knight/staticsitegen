601c0f6cac3d3a5a523139de621bed65bb48830c
walther_knight <knightofargh@gmail.com>
2025-02-18 15:41:04 -0500

    textnode to HTML conversion

--- src/main.py
+++ src/main.py
@@ -2,9 +2,18 @@ from textnode import *
 from htmlnode import *
 
 def main():
-    node = TextNode("Text Content", TextType.ITALIC)
+    text = "This is normal text"
+    text_type = TextType.TEXT
+    url = {"src": "https://imgur.com/adhdad35f5", "alt": "Alternate Text"}
+    
+    text_node = TextNode(text, text_type, url)
+    
+    def test_link_text_node():
+        node = TextNode("click here", TextType.LINK, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        print(html_node)
+    # What properties would you expect html_node to have?
 
-
-    print(node)
+    test_link_text_node()
 
 main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,9 +1,10 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
+#test cases for TextNode base function
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
@@ -42,7 +43,72 @@ class TestTextNode(unittest.TestCase):
     def test_url_none(self):
         node = TextNode("This is a text node", TextType.BOLD, None)
 
+#Tests for TextNode to HTMLNode conversion
+    def test_normal_text(self):
+        text = "This is normal text"
+        text_type = TextType.TEXT
+        url = None
+    
+        text_node = TextNode(text, text_type, url)
+    
+        self.assertEqual(
+            str(text_node_to_html_node(text_node)), 'LeafNode(Tag: None, Value: This is normal text, Props: None)'
+        )
 
+    def test_bold_text(self):
+        text = "This is bold text"
+        text_type = TextType.BOLD
+        url = None
+    
+        text_node = TextNode(text, text_type, url)
+    
+        self.assertEqual(
+            str(text_node_to_html_node(text_node)), 'LeafNode(Tag: b, Value: This is bold text, Props: None)'
+        )
+
+    def test_italic_text(self):
+        text = "This is italic text"
+        text_type = TextType.ITALIC
+        url = None
+    
+        text_node = TextNode(text, text_type, url)
+    
+        self.assertEqual(
+            str(text_node_to_html_node(text_node)), 'LeafNode(Tag: i, Value: This is italic text, Props: None)'
+        )
+
+    def test_code_block(self):
+        text = "This is a code block"
+        text_type = TextType.CODE
+        url = None
+    
+        text_node = TextNode(text, text_type, url)
+    
+        self.assertEqual(
+            str(text_node_to_html_node(text_node)), 'LeafNode(Tag: code, Value: This is a code block, Props: None)'
+        )
+
+    def test_link(self):
+        text = "This is a link"
+        text_type = TextType.LINK
+        url = "https://imgur.com/adhdad35f5"
+    
+        text_node = TextNode(text, text_type, url)
+    
+        self.assertEqual(
+            str(text_node_to_html_node(text_node)), "LeafNode(Tag: a, Value: This is a link, Props: {'href': 'https://imgur.com/adhdad35f5'})"
+        )
+
+    def test_image(self):
+        text = "Alternate Text"
+        text_type = TextType.IMAGE
+        url = "https://imgur.com/adhdad35f5"
+    
+        text_node = TextNode(text, text_type, url)
+    
+        self.assertEqual(
+            str(text_node_to_html_node(text_node)), "LeafNode(Tag: img, Value: , Props: {'src': 'https://imgur.com/adhdad35f5', 'alt': 'Alternate Text'})"
+        )
 
 if __name__ == "__main__":
     unittest.main()
