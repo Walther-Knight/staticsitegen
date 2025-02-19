858a4990849372fdf37e0dec7129f9ce2e1f4452
walther_knight <knightofargh@gmail.com>
2025-02-19 12:24:49 -0500

    update to code

--- src/main.py
+++ src/main.py
@@ -2,54 +2,43 @@ from textnode import *
 from htmlnode import *
 
 def main():
-    text = "Link text [link](https://google.com)."
-    text_type = TextType.IMAGE
+    text = "This is **bold** text."
+    text_type = TextType.TEXT
     url = {"href": "https://google.com"}
     
-    text_node = TextNode(text, text_type, url)
+    text_node = [TextNode("This is *italic* text.", TextType.TEXT), TextNode("Very *italic*, much text, wow.", TextType.TEXT)]
+    text_node1 = TextNode(text, text_type, url)
+    old_nodes = [text_node, text_node1]
     #output = (text_node_to_html_node(text_node))
     #print(output.tag)
     #print(output.to_html())
 
     def split_nodes_delimiter(old_nodes, delimiter, text_type):
-        text_list = old_nodes.text.split(" ")
-        print(text_list)
         new_nodes = []
-        delimit_start = None
-        delimit_end = None
-        if delimiter == "[link]":
-            for word in range(len(text_list)):
-                if text_list[word].startswith(delimiter):
-                    delimit_start = word
-            new_nodes.append(TextNode(" ".join(text_list[0:delimit_start]), TextType.TEXT, None))
-            new_nodes.append(TextNode(" ".join(text_list[delimit_start:delimit_start + 1]).replace(delimiter, "").replace("(", "").replace(")", ""), text_type, url))
-            if text_list[delimit_start + 1:]:
-                new_nodes.append(TextNode(" ".join(text_list[delimit_start + 1:]), TextType.TEXT, None))
-            return new_nodes
-        if delimiter == "!":
-            print("STUFF")
-            return "STUFF"
-        
-        for word in range(len(text_list)):
-            if text_list[word].startswith(delimiter):
-                delimit_start = word
-            if text_list[word].endswith(delimiter):
-                delimit_end = word + 1
-        if delimit_start is not None:
-            new_nodes.append(TextNode(" ".join(text_list[0:delimit_start]), TextType.TEXT, None))
-            new_nodes.append(TextNode(" ".join(text_list[delimit_start:delimit_end]).replace(delimiter, ""), text_type, None))
-            if text_list[delimit_end:]:
-                new_nodes.append(TextNode(" ".join(text_list[delimit_end:]), TextType.TEXT, None))
-            return new_nodes
-        return old_nodes
+        for node in old_nodes:
+            delimit_start = node.text.find(delimiter)
+            if node.text_type is not TextType.TEXT:
+                new_nodes.append(node)
+            elif delimit_start != -1:
+                delimit_end = node.text.find(delimiter, delimit_start + 1)
+                if delimit_end != -1:
+                    new_nodes.append(TextNode(node.text[0:delimit_start], TextType.TEXT))
+                    new_nodes.append(TextNode(node.text[delimit_start + len(delimiter): delimit_end], text_type))
+                    new_nodes.append(TextNode(node.text[delimit_end + len(delimiter) :], TextType.TEXT))
+                else:
+                    raise Exception("Missing closing delimiter in markdown")
+            else:
+                new_nodes.append(node)
+        return new_nodes
 
 
 
 
 
-    output = split_nodes_delimiter(text_node, "[link]", TextType.LINK)
-    #print(text_node_to_html_node(output).to_html())
+    output = split_nodes_delimiter(text_node, "*", TextType.ITALIC)
+    html_out = ""
     for each in output:
-        print(text_node_to_html_node(each).to_html())
+        html_out += text_node_to_html_node(each).to_html()
+    print(html_out)
 
 main()
