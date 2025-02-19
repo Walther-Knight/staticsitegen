0c32a5902a98603eedcf827b1b189bf3804eb2fd
walther_knight <knightofargh@gmail.com>
2025-02-19 15:54:37 -0500

    total rewrite because of poorly documented requirements and lack of test cases

--- src/main.py
+++ src/main.py
@@ -1,44 +1,15 @@
 from textnode import *
 from htmlnode import *
+from texthandling import *
 
 def main():
-    text = "This is **bold** text."
-    text_type = TextType.TEXT
-    url = {"href": "https://google.com"}
-    
-    text_node = [TextNode("This is *italic* text.", TextType.TEXT), TextNode("Very *italic*, much text, wow.", TextType.TEXT)]
-    text_node1 = TextNode(text, text_type, url)
-    old_nodes = [text_node, text_node1]
-    #output = (text_node_to_html_node(text_node))
-    #print(output.tag)
-    #print(output.to_html())
+    node = TextNode("**bold** and *italic*", TextType.TEXT)
+# First pass - bold
+    nodes_after_bold = split_nodes_delimiter([node], "**", TextType.BOLD)
+# Second pass - italic
+    final_nodes = split_nodes_delimiter(nodes_after_bold, "*", TextType.ITALIC)
 
-    def split_nodes_delimiter(old_nodes, delimiter, text_type):
-        new_nodes = []
-        for node in old_nodes:
-            delimit_start = node.text.find(delimiter)
-            if node.text_type is not TextType.TEXT:
-                new_nodes.append(node)
-            elif delimit_start != -1:
-                delimit_end = node.text.find(delimiter, delimit_start + 1)
-                if delimit_end != -1:
-                    new_nodes.append(TextNode(node.text[0:delimit_start], TextType.TEXT))
-                    new_nodes.append(TextNode(node.text[delimit_start + len(delimiter): delimit_end], text_type))
-                    new_nodes.append(TextNode(node.text[delimit_end + len(delimiter) :], TextType.TEXT))
-                else:
-                    raise Exception("Missing closing delimiter in markdown")
-            else:
-                new_nodes.append(node)
-        return new_nodes
-
-
-
-
-
-    output = split_nodes_delimiter(text_node, "*", TextType.ITALIC)
-    html_out = ""
-    for each in output:
-        html_out += text_node_to_html_node(each).to_html()
-    print(html_out)
+    print(nodes_after_bold)
+    print(final_nodes)
 
 main()
---
+++ src/texthandling.py
@@ -0,0 +1,31 @@
+from htmlnode import *
+from textnode import *    
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type is not TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        text = node.text
+        while True:
+            delimit_start = text.find(delimiter)
+            if delimit_start == -1:
+                if text:
+                    new_nodes.append(TextNode(text, TextType.TEXT))
+                break
+
+            delimit_end = text.find(delimiter, delimit_start + len(delimiter))
+            
+            if delimit_end == -1:
+                raise Exception("Missing closing delimiter in markdown")
+            
+            if delimit_end != -1:
+                if delimit_start > 0:
+                    new_nodes.append(TextNode(text[:delimit_start], TextType.TEXT))
+                new_nodes.append(TextNode(text[delimit_start + len(delimiter): delimit_end], text_type))
+                text = text[delimit_end + len(delimiter):]
+            
+            
+    return new_nodes
